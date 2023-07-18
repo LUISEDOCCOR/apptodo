0ffcdc7e353bc9e3ab05161e58cc3ebce5eb4903
Luis Ed <luiseduardocegueda@gmail.com>
2023-07-18 17:20:15 -0600

    test5

--- js/script.js
+++ js/script.js
@@ -24,33 +24,45 @@ function newTask(){
         divalert.innerHTML = ""
         arraytask.push((new Task(newtask.value)))
         newtask.value = ""
-    }
-    contenedor.innerHTML = ""
+    } renderCards()
+   }
 
+   function renderCards(){
+    contenedor.innerHTML = ""
+    cards = ""
     for(var i in arraytask){
         cards += `
         <div class="card mb-3" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">Date: <b>${arraytask[i].name}</b></h5>
                 <p class="card-text">${arraytask[i].task}</p>
-                <div class="d-flex flex-wrap gap-2">
-                    <a class="btn btn-danger" onclick = "eliminar(${i})">Remove</a>
-                    <a class="btn btn-success" onclick = "eliminar(${i})">To complete</a>
+                <div class="d-flex flex-wrap gap-2 justify-content-center">
+                    <a class="btn btn-danger" onclick = "eliminar(${i}, 'r')">Remove</a>
+                    <a class="btn btn-success" onclick = "eliminar(${i}, 'c')">To complete</a>
                 </div>
             </div>
       </div>`    
 
     }
-    
     let div = document.createElement("DIV")
     div.innerHTML = cards
     contenedor.appendChild(div)
-    cards = ""
-}
+   }
 
 
 function reset(){
     window.location.reload()
+}
+
+function eliminar(i, x){
+    if(x == "c"){
+        alert("Se ha completado correctamente")
+    }else{
+        alert("Se a eliminado correctamente")
+    }
+    console.log(i)
+    delete(arraytask[i])
+    renderCards()
 }
 
 
